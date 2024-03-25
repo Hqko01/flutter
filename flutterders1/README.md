3683c7f42519f17b501943aa810ed55ba5bbb2b9
Hqko01 <hakan.yilmaz0134@hotmail.com>
2024-03-25 13:15:51 +0300

    first-commit

---
+++ flutterders1/README.md
@@ -0,0 +1,3 @@
+# flutterders1
+
+A new Flutter project.
---
+++ flutterders1/android/app/src/main/kotlin/com/example/flutterders1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterders1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
